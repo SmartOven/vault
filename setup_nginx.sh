d6f9f5465ef57a6ca3289cca3a10f6c5f64a98bc
smartoven <yarik.panteleev@gmail.com>
2024-07-01 17:32:01 +0300

    fix setup_nginx.sh

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -2,4 +2,4 @@
 yes | cp -rf vault.panteleevya.ru /etc/nginx/sites-available/vault.panteleevya.ru
 sudo ln -s /etc/nginx/sites-available/vault.panteleevya.ru /etc/nginx/sites-enabled/
 nginx -t
-service nginx reload
+systemctl restart nginx
