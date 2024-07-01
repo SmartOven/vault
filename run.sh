7311f79273efd0146bf407dc9b455fb699f2ade2
smartoven <yarik.panteleev@gmail.com>
2024-07-01 16:59:49 +0300

    fix nginx conf

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ export VAULT_ADDR='http://127.0.0.1:8200'
 yes | cp -rf vault.hcl /etc/vault.d/vault.hcl
 sudo systemctl enable vault.service
 sudo systemctl start vault.service
-yes | cp -rf vault_nginx_conf /etc/nginx/sites-enabled/vault_nginx_conf
+yes | cp -rf vault.panteleevya.ru /etc/nginx/sites-available/vault.panteleevya.ru
+sudo ln -s /etc/nginx/sites-available/vault.panteleevya.ru /etc/nginx/sites-enabled/
 service nginx reload
