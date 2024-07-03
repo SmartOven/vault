81cf7eeb542621301f9668d70315a59c3e9afbbf
smartoven <yarik.panteleev@gmail.com>
2024-07-03 20:32:38 +0300

    try to dockerize

--- README.md
+++ README.md
@@ -5,24 +5,7 @@
 https://developer.hashicorp.com/vault/tutorials/getting-started/getting-started-deploy
 
 ## How to set up
-
-1. Update system with `apt update`
-2. Install and set up `ufw`
-    - `apt install ufw`
-    - `ufw default deny incoming`
-    - `ufw default allow outgoing`
-    - `ufw allow 22/tcp`
-    - `ufw allow 22`
-    - `ufw allow 80/tcp`
-    - `ufw allow 443`
-    - `ufw allow 80,443/tcp`
-    - `ufw enable`
-    - `ufw status verbose`
-    - Don't forget to check if new ssh connections are working
-3. Install `docker` with this guide - https://docs.docker.com/engine/install/ubuntu/
-4. Install `docker-compose`
-    - `sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose`
-    - `sudo chmod +x /usr/local/bin/docker-compose`
-    - `docker-compose --version`
+1. Run `bash setup.sh`
+2. Run `docker-compose up -d`
 
 Now it is ready to use, to access it just open UI and insert Root Token that you got during the initializing the server
---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Update system and install `ufw`
+apt update && apt install -y ufw
+
+# Set default policies
+ufw default deny incoming
+ufw default allow outgoing
+
+# Allow specific ports
+ufw allow 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+
+# Enable and check the status of `ufw` set up
+ufw enable
+ufw status verbose
+
+echo "ufw has been configured successfully."
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Check docker installation
+sudo docker run hello-world
+
+echo "docker has been configured successfully."
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo "docker-compose has been configured successfully."
